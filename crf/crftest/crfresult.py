c02dbf7c314bae4ef1b012fdd9d56869041b825a
liuxiaolei319 <182150305@qq.com>
2017-08-22 12:40:33 +0800

    增加结巴分词中一些无法拆分的词

--- crf/crftest/crfresult.py
+++ crf/crftest/crfresult.py
@@ -10,6 +10,11 @@ import json
 stopwords = {} #停用词用dict存储速度会很快，如果用list存储，会很慢
 for word in open('../stop_words.txt', 'r'):
     stopwords[word.strip().decode('gbk', 'ignore').encode('utf-8')] = 1
+jieba.suggest_freq((u'反应',u'速度'),True)
+jieba.suggest_freq((u'反应速度',u'快'),True)
+jieba.suggest_freq((u'综合',u'性'),True)
+jieba.suggest_freq((u'价格',u'便宜'),True)
+jieba.suggest_freq((u'很',u'大气'),True)
 class crfresult:
     """此程序适用于windows、和linux下，没采用CRFPP接口调用"""
     @classmethod
@@ -17,8 +22,8 @@ class crfresult:
         """通过传入的list数据，返回CRF标注的结果并拼接成json格式返回"""
         connect = ""
         #把传入的list拼接成一个以句号拼接这样在分句的时候会自动把句号进行切分
-        for data in datas: connect = connect + data + "。"
-        # print connect
+        for data in datas: connect = connect + data.encode("utf-8") + "。"
+        print connect
         subsection = utile()
         resultlists = subsection.subsection(connect)
 
@@ -43,7 +48,7 @@ class crfresult:
         os.system(r'"crf_test -m model test.data > output.txt"')#调用windows exe程序
         # os.system("sh /home/hadoop/nlp/crf/exec.sh")#调用linux shell的脚步
         resultjson = self.readresult(u'output.txt')
-        # print resultjson
+        print resultjson
         return resultjson
 
     # 按照CRF的格式把数据写入txt
@@ -82,7 +87,7 @@ class crfresult:
         return json.dumps(resultlists)
 if __name__ == '__main__':
     datas = []
-    datas.append("手机整体不错，只是信号有时比较弱,这个价格的手机已经算性价比高的了")
-    datas.append("很好用的一款手机，联想品牌真心不错，希望以后都能这样好下去，继续好用，一直好用")
+    datas.append(u"手机整体不错，只是信号有时比较弱,这个价格的手机已经算性价比高的了")
+    datas.append(u"很好用的一款手机，联想品牌真心不错，希望以后都能这样好下去，继续好用，一直好用")
     crfresult.crftext(datas,'test.data')
     # 物流是比较给力
